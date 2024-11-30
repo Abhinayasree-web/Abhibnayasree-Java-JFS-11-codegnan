9327df7f074b95934b279f80b0b3775183f5d5e2
Abhinayasree-web <rudrapuabhinayasree13@gmail.com>
2024-11-30 08:32:31 +0530

    Add files via upload

---
+++ webapp-11/src/main/java/com/codegnan/web/LoginServlet.java
@@ -0,0 +1,40 @@
+package com.codegnan.web;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		String uname=request.getParameter("uname");
+		String upwd=request.getParameter("upwd");
+		RequestDispatcher requestDispatcher=null;
+		if(uname.equals("abhi")&&upwd.equals("abhi123")) {
+			requestDispatcher=request.getRequestDispatcher("/success.html");
+			requestDispatcher.forward(request, response);
+		}else {
+			requestDispatcher=request.getRequestDispatcher("/failure.html");
+			requestDispatcher.forward(request, response);
+		}
+		
+		
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
